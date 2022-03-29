2f84052953571cddd7ba6e5d1ffc769e682cfafd
Miraculous Owonubi <omiraculous@gmail.com>
2022-03-29 18:07:33 +0100

    add checksum validation

--- matchr.js
+++ matchr.js
@@ -1,28 +1,24 @@
 let fs = require('fs');
 let path = require('path');
 
+let bip39 = require('bip39');
 let pms = require('pretty-ms');
 let nsp = require('near-seed-phrase');
 
 let days = require('./days');
 let {permute} = require('./utils');
 
-let bip39 = fs
-  .readFileSync('./bip39.txt')
-  .toString()
-  .split('\n')
-  .filter(Boolean)
-  .map(s => s.trim());
-
 console.log('(showing valid words only)');
 
 let valids = [];
+
+let bip39English = bip39.wordlists.english;
 
 for (let [day, guesses] of Object.entries(days)) {
   console.log(`\n[Day ${+day + 1}]`);
   // if (nValid = (matches = bip39.filter(word => word.startsWith(guess))).length) {
   for (let [guess, matches, nValid, slot] of guesses.map(Array))
-    if ((nValid = (matches = bip39.filter(word => word == guess)).length)) {
+    if ((nValid = (matches = bip39English.filter(word => word == guess)).length)) {
       console.log(`  \u2022 \x1b[36m${guess}\x1b[0m`);
       ((slot = valids[day]) || (valids.push((slot = [])), slot)).push(...matches);
       if (nValid > 1 || !matches.includes(guess)) console.log(matches.map(word => `    - \x1b[32m${word}\x1b[0m`).join('\n'));
@@ -42,11 +38,13 @@ let [current, lastWrite, now, start, total] = [0, 0, 0, Date.now(), valids.reduc
 let ticker = setInterval(() => ([current, start] = [0, Date.now()]), 5000);
 for (let potential_phrase of permute(valids)) {
   let phrase = potential_phrase.join(' ');
+  let isValid = bip39.validateMnemonic(phrase, bip39English);
   let keyPair =
-    state[phrase] ||
-    (state[phrase] = Object.fromEntries(
-      Object.entries(nsp.parseSeedPhrase(phrase)).filter(([k]) => ['secretKey', 'publicKey'].includes(k)),
-    ));
+    isValid &&
+    (state[phrase] ||
+      (state[phrase] = Object.fromEntries(
+        Object.entries(nsp.parseSeedPhrase(phrase)).filter(([k]) => ['secretKey', 'publicKey'].includes(k)),
+      )));
   let kps = Math.round((current += 1) / ((Date.now() - start) / 1000));
   let barSize = process.stdout.columns / 4;
   let spinner = ['/', '-', '\\', '-'].find((_, i) => i > (current % 27) / 9 - 1);
@@ -61,7 +59,7 @@ for (let potential_phrase of permute(valids)) {
     lastWrite = now;
     fs.writeFileSync(cacheFile, JSON.stringify(state));
   }
-  if (expectedPublicKeys.includes(keyPair.publicKey)) {
+  if (keyPair && expectedPublicKeys.includes(keyPair.publicKey)) {
     process.stdout.write(status);
     console.log('\n\x1b[32mFound Valid Key!\x1b[0m');
     console.log(` • Seed Phrase: \x1b[33m${phrase}\x1b[0m`);
