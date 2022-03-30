4d8115fd288a19c89a45513596b6ee0a137b02e1
Miraculous Owonubi <omiraculous@gmail.com>
2022-03-30 22:57:35 +0100

    cleanup: tick progress every 200ms, move guesslist

--- matchr.js
+++ matchr.js
@@ -5,8 +5,8 @@ let bip39 = require('bip39');
 let pms = require('pretty-ms');
 let nsp = require('near-seed-phrase');
 
-let days = require('./days');
 let {permute} = require('./utils');
+let {guesses: days, expectedPublicKeys} = require('./inputs');
 
 console.log('(showing valid words only)');
 
@@ -16,7 +16,6 @@ let bip39English = bip39.wordlists.english;
 
 for (let [day, guesses] of Object.entries(days)) {
   console.log(`\n[Day ${+day + 1}]`);
-  // if (nValid = (matches = bip39.filter(word => word.startsWith(guess))).length) {
   for (let [guess, matches, nValid, slot] of guesses.map(Array))
     if ((nValid = (matches = bip39English.filter(word => word == guess)).length)) {
       console.log(`  \u2022 \x1b[36m${guess}\x1b[0m`);
@@ -25,20 +24,15 @@ for (let [day, guesses] of Object.entries(days)) {
     }
 }
 
-let expectedPublicKeys = [
-  // these are the two FullAccess keys on the puzzlemaster.near
-  // account as at block hash AUuoUf2hZBWoxGF7bDuPzuyM88NHSEEr63sKgUQoHMLz
-  'ed25519:43oLtgANKwkXnyEGuCb1McGZoAydJQ56ykXcx6wDnCnK',
-  'ed25519:BTyLuYU4GzZEEN6wDPv3m1p14wLxhwnNFcymqrzNZrib',
-];
-
 console.log();
 
 let cacheFile = path.join(__dirname, '.cache.json');
 let state = fs.existsSync(cacheFile) ? JSON.parse(fs.readFileSync(cacheFile).toString()) : {};
-let [current, lastWrite, now, start, total] = [0, 0, 0, Date.now(), valids.reduce((a, t) => a * t.length, 1)];
-let ticker = setInterval(() => ([current, start] = [0, Date.now()]), 5000);
+let [current, start, total] = [0, Date.now(), valids.reduce((a, t) => a * t.length, 1)];
+let [lastWrite, lastTicked, spinCursor] = [Date.now(), 0, 0];
+let found, now;
 for (let potential_phrase of permute(valids)) {
+  current += 1;
   let phrase = potential_phrase.join(' ');
   let isValid = bip39.validateMnemonic(phrase, bip39English);
   let keyPair =
@@ -47,32 +41,34 @@ for (let potential_phrase of permute(valids)) {
       (state[phrase] = Object.fromEntries(
         Object.entries(nsp.parseSeedPhrase(phrase)).filter(([k]) => ['secretKey', 'publicKey'].includes(k)),
       )));
-  let kps = Math.round((current += 1) / ((Date.now() - start) / 1000));
-  let barSize = process.stdout.columns / 4;
-  let spinner = ['/', '-', '\\', '-'].find((_, i) => i > (current % 27) / 9 - 1);
-  let bar = Array.apply(null, {length: barSize})
-    .map((_, i) => (i < (current / total) * barSize ? '\x1b[38;5;249m\u2588' : '\x1b[48;5;238m '))
-    .join('');
-  let progress = ((current / total) * 100).toFixed(2);
-  let status = `${spinner} [${bar}\x1b[0m] (${kps} k/s) (${progress}%) (${pms(
-    ((total - current) / kps) * 1000,
-  )}) [${current}/${total}]`;
   if ((now = Date.now()) - lastWrite > 5000) {
     lastWrite = now;
     fs.writeFileSync(cacheFile, JSON.stringify(state));
   }
-  if (keyPair && expectedPublicKeys.includes(keyPair.publicKey)) {
+  found = keyPair && expectedPublicKeys.includes(keyPair.publicKey);
+  if (found || (now = Date.now()) - lastTicked > 200) {
+    lastTicked = now;
+    let barSize = process.stdout.columns / 4;
+    let kps = Math.round(current / ((Date.now() - start) / 1000));
+    let spinner = ['/', '-', '\\', '-'][(spinCursor = (spinCursor + 1) % 4)];
+    let bar = Array.apply(null, {length: barSize})
+      .map((_, i) => (i < (current / total) * barSize ? '\x1b[38;5;249m\u2588' : '\x1b[48;5;238m '))
+      .join('');
+    let progress = ((current / total) * 100).toFixed(2);
+    let status = `${spinner} [${bar}\x1b[0m] (${kps} k/s) (${progress}%) (${pms(
+      ((total - current) / kps) * 1000,
+    )}) [${current}/${total}]`;
+    let line = potential_phrase.map(word => word.padEnd(8, ' ')).join(' ');
+    console.log(`${current - 1 ? '\x1b[F\x1b[J' : ''}  \x1b[38;5;244m${line}\x1b[0m`);
     process.stdout.write(status);
-    console.log('\n\x1b[32mFound Valid Key!\x1b[0m');
+  }
+  if (found) {
+    let elapsed = pms(Date.now() - start);
+    console.log(`\n\x1b[32m> Found Valid Key In ${elapsed} <\x1b[0m`);
     console.log(` • Seed Phrase: \x1b[33m${phrase}\x1b[0m`);
-    console.log(` • Secret Key: \x1b[33m${keyPair.secretKey}\x1b[0m`);
-    console.log(` • Public Key: \x1b[33m${keyPair.publicKey}\x1b[0m`);
+    console.log(` • Secret Key : \x1b[33m${keyPair.secretKey}\x1b[0m`);
+    console.log(` • Public Key : \x1b[33m${keyPair.publicKey}\x1b[0m`);
     break;
   }
-  let line = potential_phrase.map(word => word.padEnd(8, ' ')).join(' ');
-  console.log(`${current - 1 ? '\x1b[F\x1b[0J' : ''}  \x1b[38;5;244m${line}\x1b[0m`);
-  // console.log(`\x1b[0G\x1b[2K  \x1b[38;5;244m✗ ${line}\x1b[0m`);
-  process.stdout.write(status);
 }
-console.log('\n\x1b[31mNo valid combinations found.\x1b[0m');
-clearInterval(ticker);
+if (!found) console.log('\n\x1b[31mNo valid combinations found.\x1b[0m');
