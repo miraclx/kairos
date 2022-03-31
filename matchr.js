ad1b271690014f86fc1e73c70514895fa1d7f5ae
Miraculous Owonubi <omiraculous@gmail.com>
2022-03-31 01:08:28 +0100

    refactor, reset kps counter every 5 seconds for a more accurate reading

--- matchr.js
+++ matchr.js
@@ -28,43 +28,43 @@ console.log();
 
 let cacheFile = path.join(__dirname, '.cache.json');
 let state = fs.existsSync(cacheFile) ? JSON.parse(fs.readFileSync(cacheFile).toString()) : {};
-let [current, start, total] = [0, Date.now(), valids.reduce((a, t) => a * t.length, 1)];
-let [lastWrite, lastTicked, spinCursor] = [Date.now(), 0, 0];
+let [index, startTime, total] = [0, Date.now(), valids.reduce((a, t) => a * t.length, 1)];
+let [epoch5s, kps, lastTracked, epoch2ms, spinCursor] = [Date.now(), 0, 0, 0, 0];
 let found, now;
 for (let potential_phrase of permute(valids)) {
-  current += 1;
   let phrase = potential_phrase.join(' ');
-  let isValid = bip39.validateMnemonic(phrase, bip39English);
-  let keyPair =
-    isValid &&
+  let keyPair = (state[phrase] =
+    bip39.validateMnemonic(phrase, bip39English) &&
     (state[phrase] ||
-      (state[phrase] = Object.fromEntries(
-        Object.entries(nsp.parseSeedPhrase(phrase)).filter(([k]) => ['secretKey', 'publicKey'].includes(k)),
-      )));
-  if ((now = Date.now()) - lastWrite > 5000) {
-    lastWrite = now;
+      Object.fromEntries(Object.entries(nsp.parseSeedPhrase(phrase)).filter(([k]) => ['secretKey', 'publicKey'].includes(k)))));
+  if ((now = Date.now()) - epoch5s > 5000) {
+    // do this at every 5second interval
+    epoch5s = now;
+    lastTracked = index;
     fs.writeFileSync(cacheFile, JSON.stringify(state));
   }
+  index += 1;
   found = keyPair && expectedPublicKeys.includes(keyPair.publicKey);
-  if (found || (now = Date.now()) - lastTicked > 200) {
-    lastTicked = now;
+  if (found || (now = Date.now()) - epoch2ms > 200) {
+    // do this at every 200ms interval
+    epoch2ms = now;
     let barSize = process.stdout.columns / 4;
-    let kps = Math.round(current / ((Date.now() - start) / 1000));
+    if (Date.now() - epoch5s >= 500) kps = Math.round((index - lastTracked) / ((Date.now() - epoch5s) / 1000));
     let spinner = ['/', '-', '\\', '-'][(spinCursor = (spinCursor + 1) % 4)];
     let bar = Array.apply(null, {length: barSize})
-      .map((_, i) => (i < (current / total) * barSize ? '\x1b[38;5;249m\u2588' : '\x1b[48;5;238m '))
+      .map((_, i) => (i < (index / total) * barSize ? '\x1b[38;5;249m\u2588' : '\x1b[48;5;238m '))
       .join('');
-    let progress = ((current / total) * 100).toFixed(2);
+    let progress = ((index / total) * 100).toFixed(2);
     let status = `${spinner} [${bar}\x1b[0m] (${kps} k/s) (${progress}%) (${pms(
-      ((total - current) / kps) * 1000,
-    )}) [${current}/${total}]`;
+      ((total - index) / (kps || 1)) * 1000,
+    )}) [${index}/${total}]`;
     let line = potential_phrase.map(word => word.padEnd(8, ' ')).join(' ');
-    console.log(`${current - 1 ? '\x1b[F\x1b[J' : ''}  \x1b[38;5;244m${line}\x1b[0m`);
+    console.log(`${index - 1 ? '\x1b[F\x1b[J' : ''}  \x1b[38;5;244m${line}\x1b[0m`);
     process.stdout.write(status);
   }
   if (found) {
-    let elapsed = pms(Date.now() - start);
-    console.log(`\n\x1b[32m> Found Valid Key In ${elapsed} <\x1b[0m`);
+    let elapsed = pms(Date.now() - startTime);
+    console.log(`\n\x1b[32m> Found Valid Key (in ${elapsed}) <\x1b[0m`);
     console.log(` • Seed Phrase: \x1b[33m${phrase}\x1b[0m`);
     console.log(` • Secret Key : \x1b[33m${keyPair.secretKey}\x1b[0m`);
     console.log(` • Public Key : \x1b[33m${keyPair.publicKey}\x1b[0m`);
